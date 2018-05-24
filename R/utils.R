321104e904d9d1b2e60415b86ca9cf1edff715da
Martin Petr <git@bodkan.net>
2018-05-24 11:31:19 +0200

    Add binning functions

--- R/utils.R
+++ R/utils.R
@@ -12,6 +12,7 @@ set_dim <- function(width, height, res = 150) {
           repr.plot.res = res)
 }
 
+
 #' Convience function that is a complement to \%in\%.
 #'
 #' @param x Vector of elements to be matched.
@@ -23,6 +24,7 @@ set_dim <- function(width, height, res = 150) {
 '%notin%' <- function(x, vec) {
   match(x, vec, nomatch = 0L) == 0L
 }
+
 
 #' Paste two strings using an infix operator.
 #'
@@ -36,12 +38,40 @@ set_dim <- function(width, height, res = 150) {
   paste0(a, b)
 }
 
+
 #' Shortcut for suppressMessages().
 #' @param expr Expression to evaluate.
 #' @export
 sm <- function(expr) suppressMessages(expr)
 
+
 #' Shortcut for suppressPackageStartupMessages().
 #' @param expr Expression to evaluate.
 #' @export
 ssm <- function(expr) suppressPackageStartupMessages(expr)
+
+
+#' Split values into discrete, equal sized bins.
+#'
+#' @param x Values to bin.
+#' @param n Number of bins.
+#' @param num_labels Label the bins with simple sequence of numbers?
+#'
+#' @return Factor of bins.
+bin_quantiles <- function(x, n, num_labels = FALSE) {
+  breaks <- quantile(x, seq(0, 1, 1 / n))
+  labels = if (num_labels) 1 : n else NULL
+  cut(x, breaks, levels = breaks, include.lowest = TRUE, labels = labels)
+}
+
+
+#' Split values into discrete bins.
+#'
+#' @param x Values to bin.
+#' @param breaks A vector of cut points or a single number determining
+#'     the number of bins.
+#'
+#' @return Factor of bins.
+bin_values <- function(x, breaks) {
+  cut(x, breaks, levels = breaks, include.lowest = TRUE)
+}
